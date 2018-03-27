59c2dc0b840991a1219f353732a7bd6c0e425340
phemy <phemy.smith@gmail.com>
2018-03-27 11:16:57 +0100

    setting up routes babel and eslint

---
+++ server/Server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+// import api from './routes/api'
+
+// Instance of the express app
+const app = express();
+
+//Request logger
+app.use(morgan('dev'));
+
+// Parsing body data
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+app.get('/',  (req, res) => {
+    res.status(200).send({
+        message: 'Welcome to connect we',
+    });
+});
+
+
+const port = process.env.PORT || 7000;
+
+app.listen(port);
+
+console.log(`Find me on http://localhost:${port}`);
